60b1d913c3df47e376d2b8dbecc34a876f85ae67
unknown <jdavid.stevens@gmail.com>
2018-10-12 10:34:11 -0600

    Pre_img_move

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ const axios = require('axios');
 const app=express();
 
 let {
-    SERVER_PORT
+    SERVER_PORT,
+    CONNECTION_STRING
 }= process.env
+
+massive(CONNECTION_STRING).then(dbInstance=>{
+    app.set('db',dbInstance)
+})
 
 app.listen(SERVER_PORT, ()=>{
     console.log(`Live on port ${SERVER_PORT}` )
